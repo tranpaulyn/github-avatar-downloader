a8360deaa0cb4da0a3092945575cfb3a6aec59a8
Paulyn <paulyntran@live.ca>
2019-02-12 02:41:58 +0000

    J.SON parse and added console.log for each avatarURL

--- download_avatars.js
+++ download_avatars.js
@@ -8,19 +8,30 @@ var token = require('./secrets.js')
 // the callback handles asynchronous nature of results that are returned from getRepoContributors
 function getRepoContributors(repoOwner, repoName, cb) {
     var options = {
-        url: "https://api.github.com/repos" + repoOwner + "/" + repoName + "/contributors",
+        url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
         headers: {
             'User-Agent': 'request',
             'Authorization': token
         }
     };
-
+    // parse the JSON string into an object and pass this object to the cb function
     request(options, function(err, res, body) {
-        cb(err,body);
+        cb(err, JSON.parse(body));
     });
+    
 }
+
 
 getRepoContributors("jquery", "jquery", function(err, result) {
     console.log("Errors:", err);
     console.log("Result:", result);
+    // accessing the object
+    var user = []
+    // going through every user access the user avatar url
+    for (var i = 0; i < result.length; i++) {
+        user = result[i].login;
+        var avatarURL = result[i].avatar_url;
+        console.log(user);
+        console.log(avatarURL);
+    }
   });
