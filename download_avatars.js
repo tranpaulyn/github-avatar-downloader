beb3360a4321f8f0635c7f27bf47ceb0fe69f0e7
Paulyn <paulyntran@live.ca>
2019-02-12 16:36:45 +0000

    Completed download avatars with command line arguments

--- download_avatars.js
+++ download_avatars.js
@@ -1,38 +1,50 @@
 var request = require('request');
 var fs = require('fs');
+var repo = process.argv.slice(2)
 
+var owner = repo[0];
+var name = repo[1];
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 var token = require('./secrets.js')
 
+
 // Use the request library to progromatically fetch the list of contributors via HTTPS for the given repo
 // the callback handles asynchronous nature of results that are returned from getRepoContributors
-function getRepoContributors(repoOwner, repoName, cb) {
-    var options = {
-        url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-        headers: {
-            'User-Agent': 'request',
-            'Authorization': token
-        }
-    };
-    // parse the JSON string into an object and pass this object to the cb function
-    request(options, function(err, res, body) {
-        cb(err, JSON.parse(body));
-    });
-    
+
+
+if (owner === [] || name === []) {
+console.log("Please enter repository information")
+} else {
+    function getRepoContributors(repoOwner, repoName, cb) {
+        var options = {
+            url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+            headers: {
+                'User-Agent': 'request',
+                'Authorization': token
+            }
+        };
+        // parse the JSON string into an object and pass this object to the cb function
+        request(options, function(err, res, body) {
+            cb(err, JSON.parse(body));
+        });
+        
+    }    
+
 }
 
 function downloadImageByURL(url, filePath) {
     request.get(url)
         .on('response', function(res) {
-            console.log(res)
+            // console.log(res)
         })
         .pipe(fs.createWriteStream(filePath))
 }
 
 
-getRepoContributors("jquery", "jquery", function(err, result) {
+getRepoContributors(owner, name, function(err, result) {
     console.log("Errors:", err);
+    console.log("Please enter valid information")
     // console.log("Result:", result);
     // accessing the object
     // going through every user access the user avatar url
